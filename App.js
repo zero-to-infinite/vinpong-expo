85f1540d661e32a419f1ef6ed7028d537389e735
gullgae <pjw9819@naver.com>
2023-03-28 23:24:20 +0900

    second commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>뿌웅</Text>
+      <Text>vintage Pingpong</Text>
       <StatusBar style="auto" />
     </View>
   );
