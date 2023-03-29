6a487440147656ea77ecc8cf958e7d50a0c6d0c0
gullgae <pjw9819@naver.com>
2023-03-30 00:33:28 +0900

    login

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Loading() {
   return (
@@ -10,11 +11,39 @@ export default function Loading() {
       <View>
         <Text style={vvp.container}> Vintage Ping Pong </Text>
       </View>
+
+      <TouchableOpacity style={content.container}>
+	      <Text style={login.container}>
+		      login
+	      </Text>
+
+      </TouchableOpacity>
+
+      
       
     </View>
   );
 }
 
+const content = StyleSheet.create({
+  container: {
+    backgroundColor: '#91B391', 
+	  borderRadius: 25, // 테두리를 둥글게 만드는 속성(숫자가 클수록 타원에 가까워짐)
+	  marginHorizontal: 20, 
+	  marginBottom: 12, 
+	  paddingHorizontal: 40, //좌우 길이
+	  paddingVertical: 10, 
+	  alignItems: "center", 
+  }
+});
+
+const login = StyleSheet.create({
+  container: {
+    color:"white",
+    fontSize: 15, 
+	  fontWeight: "600", 
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -28,7 +57,7 @@ const styles = StyleSheet.create({
 const vvp = StyleSheet.create({
   container: {
     color:"#91B391",
-    fontSize: 30, 
-	  fontWeight: "600", // 폰트 두께는 600
+    fontSize: 40, 
+	  fontWeight: "600", 
   }
 });
