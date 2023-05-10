eb0470bb36b2ea26720dbaa13371e1f0f8665530
gullgae <pjw9819@naver.com>
2023-05-10 21:23:51 +0900

    항목 누르면 표시되는거

--- screens/Search.js
+++ screens/Search.js
@@ -10,9 +10,7 @@ export default function Search() {
   const [selectedStyles, setSelectedStyles] = useState([]); // 선택된 스타일 목록
   const [selectedCategories, setSelectedCategories] = useState([]); // 선택된 카테고리 목록
   const stylesList = ["Casual", "Formal", "Sports", "Vintage", "Sportswear", "Classic", "Travel"];
-
-  const categoryList = ["전체", "상의", "하의", "신발", "악세사리","아우터"];
-
+  const categoryList = ["전체", "상의", "하의", "신발", "악세사리", "아우터"];
   const popularList = [
     { id: 1, name: "운동화" },
     { id: 2, name: "후드티" },
@@ -38,7 +36,7 @@ export default function Search() {
       setSelectedStyles([...selectedStyles, style]);
     }
   };
-  
+
   const handleCategoryOptionPress = (category) => {
     if (selectedCategories.includes(category)) {
       setSelectedCategories(selectedCategories.filter((selectedCategory) => selectedCategory !== category));
@@ -47,122 +45,104 @@ export default function Search() {
     }
   };
 
-  {isStyleOpen && (
-  <View style={[styles.options, styles.styleOptions]}>
-    {stylesList.map((style, index) => (
-      <TouchableOpacity
-        key={index}
-        style={[
-          styles.option,
-          selectedStyles.includes(style) ? styles.selectedOption : null,
-        ]}
-        onPress={() => handleStyleOptionPress(style)}
-      >
-        <Text>{style}</Text>
-        {selectedStyles.includes(style) && (
-          <Feather name="x" size={16} color="#777" style={styles.icon} />
-        )}
-      </TouchableOpacity>
-    ))}
-  </View>
-)}
-
-{isCategoryOpen && (
-  <View style={[styles.options, styles.categoryOptions]}>
-    {categoryList.map((category, index) => (
-      <TouchableOpacity
-        key={index}
-        style={[
-          styles.option,
-          selectedCategories.includes(category) ? styles.selectedOption : null,
-        ]}
-        onPress={() => handleCategoryOptionPress(category)}
-      >
-        <Text>{category}</Text>
-        {selectedCategories.includes(category) && (
-          <Feather name="x" size={16} color="#777" style={styles.icon} />
-        )}
-      </TouchableOpacity>
-    ))}
-  </View>
-)}
-
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="transparent" translucent={true} />
+  <StatusBar backgroundColor="transparent" translucent={true} />
 
-      <View style={styles.searchContainer}>
-        <TextInput style={styles.searchInput} placeholder="검색어를 입력하세요" />
-        <Feather name="search" size={24} color="#777" style={styles.icon} />
-      </View>
+  <View style={styles.searchContainer}>
+    <TextInput style={styles.searchInput} placeholder="검색어를 입력하세요" />
+    <Feather name="search" size={24} color="#777" style={styles.icon} />
+  </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.button, isStyleOpen && styles.selectedButton]} onPress={handleStylePress}>
-          <Text style={styles.buttonText}>Style</Text>
-        </TouchableOpacity>
+  <View style={styles.buttonContainer}>
+    <TouchableOpacity
+      style={[styles.button, isStyleOpen && styles.selectedButton]}
+      onPress={handleStylePress}
+    >
+      <Text style={styles.buttonText}>Style</Text>
+    </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.button, isCategoryOpen && styles.selectedButton]} onPress={handleCategoryPress}>
-          <Text style={styles.buttonText}>Category</Text>
-        </TouchableOpacity>
-      </View>
+    <TouchableOpacity
+      style={[styles.button, isCategoryOpen && styles.selectedButton]}
+      onPress={handleCategoryPress}
+    >
+      <Text style={styles.buttonText}>Category</Text>
+    </TouchableOpacity>
+  </View>
 
-      {isStyleOpen && (
-        <View style={[styles.options, styles.styleOptions]}>
-          {stylesList.map((style, index) => (
-            <TouchableOpacity key={index} style={styles.option}>
-              <Text>{style}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-      )}
-
-      {isCategoryOpen && (
-        <View style={[styles.options, styles.categoryOptions]}>
-          {categoryList.map((category, index) => (
-            <TouchableOpacity key={index} style={styles.option}>
-              <Text>{category}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-      )}
-
-      <View style={styles.popularContainer}>
-        <Text style={styles.popularTitle}>인기 검색어</Text>
-        {popularList.map((keyword, index) => (
-          <View key={index} style={styles.popularItem}>
-            <Text style={styles.popularRank}>{index + 1}</Text>
-            <Text style={styles.popularName}>{keyword.name}</Text>
-          </View>
-        ))}
-      </View>
-
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.menuIcon}>
-          <Feather name="home" size={28} color="white" />
-        </TouchableOpacity>
-
+  {isStyleOpen && (
+    <View style={[styles.options, styles.styleOptions]}>
+      {stylesList.map((style, index) => (
         <TouchableOpacity
-            style={styles.menuIcon}
-            onPress={() => navigation.navigate("Search")}>
-          <Feather name="search" size={28} color="white" />
+          key={index}
+          style={[
+            styles.option,
+            selectedStyles.includes(style) ? styles.selectedOption : null,
+          ]}
+          onPress={() => handleStyleOptionPress(style)}
+        >
+          <Text>{style}</Text>
+          {selectedStyles.includes(style) && (
+            <Feather name="x" size={16} color="#777" style={styles.icon} />
+          )}
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.menuIcon}>
-          <Feather name="plus-circle" size={28} color="white" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuIcon}>
-          <Feather name="message-square" size={28} color="white" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuIcon}>
-          <FontAwesome name="user-circle-o" size={28} color="white" />
-        </TouchableOpacity>
-      </View>
-
-
-      
+      ))}
     </View>
-  );
-}
+  )}
+
+  {isCategoryOpen && (
+    <View style={[styles.options, styles.categoryOptions]}>
+      {categoryList.map((category, index) => (
+        <TouchableOpacity
+          key={index}
+          style={[
+            styles.option,
+            selectedCategories.includes(category) ? styles.selectedOption : null,
+          ]}
+          onPress={() => handleCategoryOptionPress(category)}
+        >
+          <Text>{category}</Text>
+          {selectedCategories.includes(category) && (
+            <Feather name="x" size={16} color="#777" style={styles.icon} />
+          )}
+        </TouchableOpacity>
+      ))}
+    </View>
+  )}
+
+  <View style={styles.popularContainer}>
+    <Text style={styles.popularTitle}>인기 검색어</Text>
+    {popularList.map((keyword, index) => (
+      <View key={index} style={styles.popularItem}>
+        <Text style={styles.popularRank}>{index + 1}</Text>
+        <Text style={styles.popularName}>{keyword.name}</Text>
+      </View>
+    ))}
+  </View>
+
+  <View style={styles.footer}>
+    <TouchableOpacity style={styles.menuIcon}>
+      <Feather name="home" size={28} color="white" />
+    </TouchableOpacity>
+
+    <TouchableOpacity style={styles.menuIcon} onPress={() => navigation.navigate("Search")}>
+      <Feather name="search" size={28} color="white" />
+    </TouchableOpacity>
+
+    <TouchableOpacity style={styles.menuIcon}>
+      <Feather name="plus-circle" size={28} color="white" />
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.menuIcon}>
+      <Feather name="message-square" size={28} color="white" />
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.menuIcon}>
+      <FontAwesome name="user-circle-o" size={28} color="white" />
+    </TouchableOpacity>
+  </View>
+</View>
+
+);}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -211,12 +191,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee",
     paddingVertical: 5,
     paddingHorizontal: 10,
-    flex: 1, // 1:1 비율로 화면 차지
+    flex: 1,
     alignItems: "flex-start",
     marginLeft: 5,
     marginTop: 10,
   },
-  
   option: {
     flexDirection: "row",
     alignItems: "center",
@@ -226,10 +205,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: "#ddd",
   },
-  checkbox: {
-    marginRight: 10,
-  },
-  selectedButton: {
+  selectedOption: {
     backgroundColor: "#91B391",
   },
   chartContainer: {
@@ -255,25 +231,24 @@ const styles = StyleSheet.create({
   chartText: {
     fontSize: 16,
   },
-
   popularContainer: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: "#f2f2f2",
     paddingHorizontal: 16,
     paddingVertical: 8,
   },
   popularTitle: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 16,
     marginBottom: 8,
   },
   popularItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 8,
   },
   popularRank: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 14,
     marginRight: 8,
   },
