4f143d34d45611d303b66a268940369625f4199d
okxooxoo <okxooxoo@naver.com>
2023-05-13 23:16:27 +0900

    Modify SignUp.js(2차 수정)

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -14,15 +14,23 @@ export default function SignUp({ navigation }) {
   const [pw, setPw] = useState("");
   const [pwCheck, setPwCheck] = useState("");
   const [name, setName] = useState("");
-
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  /*
   const [emailMessage, setEmailMessage] = useState("");
   const [pwMessage, setPwMessage] = useState("");
   const [pwCheckMessage, setPwCheckMessage] = useState("");
   const [nameMessage, setNameMessage] = useState("");
-
+  */
   const signUp = () => {
-    if (pw !== pwCheck) {
+    if (email == "") {
+      alert("이메일은 필수 입력입니다!");
+    } else if (pw == "") {
+      alert("비밀번호는 필수 입력입니다!");
+    } else if (pw !== pwCheck) {
       alert("비밀번호가 일치하지 않습니다.");
+    } else if (name == "") {
+      alert("닉네임은 필수 입력입니다!");
     } else {
       alert(
         `가입을 축하드립니다!\n이메일: ${email}\n비밀번호: ${pw}\n닉네임: ${name}`
@@ -40,8 +48,11 @@ export default function SignUp({ navigation }) {
       </View>
 
       <View style={styles.body}>
-        <View style={styles.email}>
-          <Text style={styles.text}>이메일</Text>
+        {/* 이메일 입력창 */}
+        <View style={styles.form}>
+          <View style={styles.textContainer}>
+            <Text>이메일</Text>
+          </View>
           <TextInput
             placeholder="이메일을 입력하세요."
             onChangeText={setEmail}
@@ -49,22 +60,25 @@ export default function SignUp({ navigation }) {
             style={styles.input}
           />
           <TouchableOpacity style={styles.btn}>
-            <Text style={styles.text}>인증 요청</Text>
+            <Text>인증 요청</Text>
           </TouchableOpacity>
         </View>
-
-        <View style={styles.auth}>
+        {/* 이메일 인증 코드 입력창 */}
+        <View style={styles.form}>
           <TextInput style={styles.input} />
-          <Text style={styles.text}>4:39</Text>
+          <View style={styles.textContainer}>
+            <Text>4:39</Text>
+          </View>
           <TouchableOpacity style={styles.btn}>
-            <Text style={styles.text}>인증 확인</Text>
+            <Text>인증 확인</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.hr} />
 
-        <View style={styles.pw}>
-          <Text style={styles.text}>비밀번호</Text>
+        {/* 비밀번호 입력창 */}
+        <View style={styles.form}>
+          <Text>비밀번호</Text>
           <TextInput
             placeholder="비밀번호를 입력하세요."
             secureTextEntry={true}
@@ -73,9 +87,9 @@ export default function SignUp({ navigation }) {
             style={styles.input}
           />
         </View>
-
-        <View style={styles.pw}>
-          <Text style={styles.text}>비밀번호 확인</Text>
+        {/* 비밀번호 확인 입력창 */}
+        <View style={styles.form}>
+          <Text>비밀번호 확인</Text>
           <TextInput
             placeholder="비밀번호를 입력하세요."
             secureTextEntry={true}
@@ -87,8 +101,11 @@ export default function SignUp({ navigation }) {
 
         <View style={styles.hr} />
 
-        <View style={styles.name}>
-          <Text style={styles.text}>닉네임</Text>
+        {/* 닉네임 입력창 */}
+        <View style={styles.form}>
+          <View style={styles.textContainer}>
+            <Text>닉네임</Text>
+          </View>
           <TextInput
             placeholder="닉네임을 입력하세요."
             onChangeText={setName}
@@ -96,39 +113,43 @@ export default function SignUp({ navigation }) {
             style={styles.input}
           />
           <TouchableOpacity style={styles.btn}>
-            <Text style={styles.text}>중복 확인</Text>
+            <Text>중복 확인</Text>
           </TouchableOpacity>
         </View>
 
-        <View style={styles.pw}>
-          <Text style={styles.text}>연락처</Text>
+        {/* 연락처 입력창 */}
+        <View style={styles.form}>
+          <View style={styles.textContainer}>
+            <Text>연락처</Text>
+          </View>
           <TextInput
-            placeholder="비밀번호를 입력하세요."
-            secureTextEntry={true}
-            onChangeText={setPw}
-            value={pw}
+            placeholder="휴대폰 번호를 입력하세요."
+            onChangeText={setPhone}
+            value={phone}
             style={styles.input}
           />
           <TouchableOpacity style={styles.btn}>
-            <Text style={styles.text}>인증 요청</Text>
+            <Text>인증 요청</Text>
           </TouchableOpacity>
         </View>
 
-        <View style={styles.pw}>
-          <Text style={styles.text}>주소</Text>
+        {/* 주소 입력창 */}
+        <View style={styles.form}>
+          <View style={styles.textContainer}>
+            <Text>주소</Text>
+          </View>
           <TextInput
-            placeholder="비밀번호를 입력하세요."
-            secureTextEntry={true}
-            onChangeText={setPw}
-            value={pw}
+            placeholder="주소를 입력하세요."
+            onChangeText={setAddress}
+            value={address}
             style={styles.input}
           />
           <Feather name="search" size={24} color="#bbb" style={styles.icon} />
         </View>
       </View>
 
-      <TouchableOpacity onPress={signUp} style={styles.signUp}>
-        <Text style={styles.text}>회원가입</Text>
+      <TouchableOpacity onPress={signUp} style={styles.signUpBtn}>
+        <Text>회원가입</Text>
       </TouchableOpacity>
     </View>
   );
@@ -160,34 +181,14 @@ const styles = StyleSheet.create({
   body: {
     width: "100%",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
 
-  email: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginHorizontal: 15,
-    marginVertical: 5,
-  },
-
-  auth: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginHorizontal: 15,
-    marginVertical: 5,
-  },
-
-  pw: {
+  form: {
+    width: "90%",
+    justifyContent: "space-between",
     alignItems: "center",
     flexDirection: "row",
-    marginHorizontal: 15,
-    marginVertical: 5,
-  },
-
-  name: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginHorizontal: 15,
     marginVertical: 5,
   },
 
@@ -197,29 +198,27 @@ const styles = StyleSheet.create({
     borderColor: "#91B391",
     borderWidth: 1,
     padding: 10,
-    marginHorizontal: 5,
+    marginHorizontal: 6,
   },
 
   btn: {
     backgroundColor: "#91B391",
-    borderColor: "#91B391",
-    borderWidth: 1,
     borderRadius: 20,
     padding: 10,
-    marginHorizontal: 5,
+    marginHorizontal: 3,
   },
 
-  text: {
-    marginHorizontal: 5,
+  textContainer: {
+    width: 40,
+    alignItems: "center",
   },
 
-  signUp: {
+  signUpBtn: {
     backgroundColor: "#91B391",
     borderColor: "#91B391",
     borderWidth: 1,
-    borderRadius: 20,
+    borderRadius: 10,
     padding: 12,
-    marginTop: 20,
     marginBottom: 50,
   },
 
