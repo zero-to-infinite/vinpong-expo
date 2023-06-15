9ee81223ec9d7e2775a90e4ba85655facec72067
okxooxoo <okxooxoo@naver.com>
2023-06-15 23:40:35 +0900

    Home 화면에서 현재 위치, 온도, 날씨, 옷차림 추천 구현

--- screens/Home.js
+++ screens/Home.js
@@ -7,20 +7,117 @@ import {
   ScrollView,
   Dimensions,
   Image,
+  ActivityIndicator,
 } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import BottomNav from "../components/BottomNav";
 import TopBar from "../components/TopBar";
 import { AntDesign } from "@expo/vector-icons";
 import { getAllImages, getAllUserImages } from "../services/storage";
-import { getUserInfo } from "../services/firestore_user";
+import * as Location from "expo-location";
 import styles from "../styles/HomeStyles";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "9aebfd2a3e3ba2172d0b6aa7582d3ca9"; // openweathermap api key
+
 export default function Home({ navigation }) {
-  const [productImages, setProductImages] = useState([]);
-  const [storeImages, setStoreImages] = useState([]);
+  const [productImages, setProductImages] = useState([]); // 홈에 보일 상품 이미지들
+  const [storeImages, setStoreImages] = useState([]); // 홈에 보일 상점 이미지들
+
+  const [city, setCity] = useState(""); // 현재 위치한 도시
+  const [weather, setWeather] = useState(""); // 오늘 날씨
+  const [temp, setTemp] = useState(""); // 현재 온도
+
+  const [ok, setOk] = useState(true); // 위치 정보 수집 허용 여부
+
+  const recommendDress = (temp, weather) => {
+    if (temp < 5) {
+      var text = "패딩과 두꺼운 코트, 목도리를 추천합니다 :)";
+    } else if (5 <= temp && temp < 9) {
+      var text = "코트, 히트텍, 두꺼운 니트를 추천합니다 :)";
+    } else if (9 <= temp && temp < 12) {
+      var text = "트렌치코트, 자켓, 니트를 추천합니다 :)";
+    } else if (12 <= temp && temp < 17) {
+      var text = "자켓, 가디건, 조끼를 추천합니다 :)";
+    } else if (17 <= temp && temp < 20) {
+      var text = "얇은 니트, 맨투맨, 후드티를 추천합니다 :)";
+    } else if (20 <= temp && temp < 23) {
+      var text = "셔츠, 긴팔티, 청바지를 추천합니다 :)";
+    } else if (23 <= temp && temp < 28) {
+      var text = "반팔티, 반바지, 얇은 셔츠를 추천합니다 :)";
+    } else if (28 <= temp) {
+      var text = "민소매, 반팔, 반바지를 추천합니다 :)";
+    }
+
+    var weatherLower = weather.toLowerCase();
+
+    if (weatherLower.includes("clear")) {
+      var icon = "☀️";
+    } else if (weatherLower.includes("rain")) {
+      var icon = "🌧️";
+    } else if (weatherLower.includes("cloudes")) {
+      var icon = "💨";
+    } else if (weatherLower.includes("thunderstorm")) {
+      var icon = "⚡️";
+    } else if (weatherLower.includes("snow")) {
+      var icon = "❄️";
+    } else if (weatherLower.includes("mist")) {
+      var icon = "🌫️";
+    } else {
+      var icon = "☺️";
+    }
+    return (
+      <View style={styles.weatherInner}>
+        <View style={styles.weatherLeft}>
+          <Text style={styles.city}>{city}</Text>
+          <Text style={styles.weather}>{weather}</Text>
+          <Text style={styles.temp}>{Math.round(temp)}°</Text>
+        </View>
+        <View style={styles.weatherRight}>
+          <Text style={styles.dressIcon}>{icon}</Text>
+          <Text style={styles.dressText}>{text}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  // 현재 유저의 위치 정보를 수집하고 날씨 정보를 가져오는 함수
+  const getWeather = async () => {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        setOk(false);
+      }
+      // 현재 위치의 위도, 경도 수집
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      // 위도, 경도를 가지고 주소지로 변환
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCity(location[0].city);
+      // 날씨 api 가져오기
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+        );
+        const json = await response.json();
+        setWeather(json.weather[0].main);
+        setTemp(json.main.temp);
+      } catch (e) {
+        console.log(e);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -48,10 +145,22 @@ export default function Home({ navigation }) {
       <View style={styles.body}>
         <Image
           source={{
-            uri: "https://firebasestorage.googleapis.com/v0/b/vinpong-3a05c.appspot.com/o/banner.jpg?alt=media&token=19f08c73-a082-4a08-8ce8-87831d7b8ceb&_gl=1*1x55dtp*_ga*MjAzNjMwMTE2Mi4xNjgzODkxNjAx*_ga_CW55HF8NVT*MTY4NjQ5NzEyOC40MS4xLjE2ODY0OTcxNDguMC4wLjA.",
+            uri: "https://firebasestorage.googleapis.com/v0/b/vinpong-3a05c.appspot.com/o/banner3.jpg?alt=media&token=3401f32e-8f7b-4efc-afcd-8e528485fbf6",
           }}
           style={styles.banner}
         />
+
+        <View
+          style={styles.weatherBox}
+        >
+          {temp === "" ? (
+            <View>
+              <ActivityIndicator color="black" size="large" />
+            </View>
+          ) : (
+            <View>{recommendDress(Math.round(temp), weather)}</View>
+          )}
+        </View>
 
         <Text style={styles.bodyText}>추천 상품</Text>
         <View style={styles.productContainer}>
@@ -66,7 +175,10 @@ export default function Home({ navigation }) {
               </TouchableOpacity>
             ))}
           </ScrollView>
-          <TouchableOpacity style={styles.scrollIcon}>
+          <TouchableOpacity
+            onPress={() => alert("오른쪽으로 스크롤을 넘겨보세요!")}
+            style={styles.scrollIcon}
+          >
             <AntDesign name="rightcircle" size={24} color="#91B391" />
           </TouchableOpacity>
         </View>
@@ -84,7 +196,10 @@ export default function Home({ navigation }) {
               </TouchableOpacity>
             ))}
           </ScrollView>
-          <TouchableOpacity style={styles.scrollIcon}>
+          <TouchableOpacity
+            onPress={() => alert("오른쪽으로 스크롤을 넘겨보세요!")}
+            style={styles.scrollIcon}
+          >
             <AntDesign name="rightcircle" size={24} color="#91B391" />
           </TouchableOpacity>
         </View>
--- styles/HomeStyles.js
+++ styles/HomeStyles.js
@@ -3,73 +3,133 @@ import { StyleSheet, Dimensions } from "react-native";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "white",
-    },
-  
-    menuBar: {
-      flexDirection: "row",
-      justifyContent: "flex-end",
-      marginTop: 50,
-      marginRight: 10,
-    },
-  
-    menuIcon: {
-      padding: 3,
-    },
-  
-    body: {
-      flex: 1,
-      marginHorizontal: 10,
-      marginVertical: 12,
-    },
-  
-    bodyText: {
-      alignSelf: "flex-start",
-      marginTop: 20,
-      marginBottom: 5,
-      marginLeft: 10,
-    },
-  
-    banner: {
-      alignItems: "center",
-      borderColor: "#669066",
-      borderWidth: 1,
-      borderRadius: 25,
-      paddingVertical: 100,
-    },
-  
-    productContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-    },
-  
-    product: {
-      justifyContent: "center",
-      alignItems: "center",
-      borderColor: "white",
-      borderWidth: 1,
-      borderRadius: 15,
-      width: (SCREEN_WIDTH - 38) * 0.3,
-      height: (SCREEN_WIDTH - 38) * 0.3,
-      margin: 3,
-    },
-  
-    scrollIcon: {
-      width: "10%",
-      alignItems: "center",
-    },
-  
-    footer: {
-      flexDirection: "row",
-      width: "100%",
-      justifyContent: "space-evenly",
-      backgroundColor: "#91B391",
-      paddingBottom: 35,
-      paddingTop: 15,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+
+  menuBar: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 50,
+    marginRight: 10,
+  },
+
+  menuIcon: {
+    padding: 3,
+  },
+
+  body: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginVertical: 12,
+  },
+
+  bodyText: {
+    alignSelf: "flex-start",
+    marginTop: 20,
+    marginBottom: 5,
+    marginLeft: 10,
+  },
+
+  banner: {
+    alignItems: "center",
+    borderColor: "#669066",
+    borderWidth: 1,
+    borderRadius: 25,
+    paddingVertical: 100,
+  },
+
+  productContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  product: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 15,
+    width: (SCREEN_WIDTH - 40) * 0.3,
+    height: (SCREEN_WIDTH - 40) * 0.3,
+    margin: 3,
+  },
+
+  scrollIcon: {
+    width: "10%",
+    alignItems: "center",
+  },
+
+  weatherBox: {
+    height: 100,
+    width: "100%",
+    //backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  weatherInner: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
+  weatherLeft: {
+    height: 100,
+    width: "27%",
+    marginRight: "1%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#A7BEDA",
+    borderRadius: 15,
+  },
+
+  weatherRight: {
+    height: 100,
+    width: "70%",
+    marginLeft: "1%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#A7BEDA",
+    borderWidth: 1,
+    borderRadius: 15,
+  },
+
+  city: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 12,
+  },
+  weather: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 12,
+  },
+  temp: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 28,
+  },
+
+  dressText: {
+    color: "#777",
+    marginTop: 7,
+  },
+
+  dressIcon: {
+    fontSize: 38,
+  },
+
+  footer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-evenly",
+    backgroundColor: "#91B391",
+    paddingBottom: 35,
+    paddingTop: 15,
+  },
+});
 
 export default styles;
